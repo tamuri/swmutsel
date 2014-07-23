0fda73270970dad8750835f5aa3b9c3c1f00834b
Asif Tamuri <tamuri@gmail.com>
2014-07-23 17:37:54 +0100

    MVN penalty as in paper.

--- src/java/swmutsel/model/MVNPenalty.java
+++ src/java/swmutsel/model/MVNPenalty.java
@@ -4,6 +4,7 @@ package swmutsel.model;
 import cern.colt.matrix.DoubleFactory1D;
 import cern.colt.matrix.DoubleMatrix1D;
 import cern.jet.math.Functions;
+import swmutsel.Constants;
 
 public class MVNPenalty implements Penalty {
 
@@ -21,7 +22,17 @@ public class MVNPenalty implements Penalty {
 
     @Override
     public double calculate(final double[] parameters) {
-        DoubleMatrix1D f = DoubleFactory1D.dense.make(parameters);
+        // 20 fitness values
+        DoubleMatrix1D f = DoubleFactory1D.dense.make(20);
+
+        // 19 parameters are estimated, passed in parameters[]
+        for (int i = 0; i < parameters.length; i++) f.setQuick(i, parameters[i]);
+
+        // One is fixed
+        f.setQuick(19, Constants.FITNESS_FIXED_FOR_RELATIVE);
+
+        double mean = f.zSum() / 20.0;
+        f.assign(Functions.minus(mean));
         return -coefficient * f.aggregate(Functions.plus, Functions.pow(2.0));
     }
 
